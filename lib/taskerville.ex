dbeb51fc81ed465241e9ffd3f6e1ae21b39a4114
Zach <zach.cw@gmail.com>
2021-02-06 11:44:02 -0600

    add mix project

---
+++ lib/taskerville.ex
@@ -0,0 +1,18 @@
+defmodule Taskerville do
+  @moduledoc """
+  Documentation for `Taskerville`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Taskerville.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/taskerville_test.exs
@@ -0,0 +1,8 @@
+defmodule TaskervilleTest do
+  use ExUnit.Case
+  doctest Taskerville
+
+  # test "greets the world" do
+  #   assert Taskerville.hello() == :world
+  # end
+end
